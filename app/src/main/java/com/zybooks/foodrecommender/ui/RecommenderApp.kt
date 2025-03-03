1c1399df32e5fca45b66b8f45995885ed27d058f
JefferNg <jefferng11@gmail.com>
2025-03-03 14:27:47 -0800

    implemented floating action refresh button

--- app/src/main/java/com/zybooks/foodrecommender/ui/RecommenderApp.kt
+++ app/src/main/java/com/zybooks/foodrecommender/ui/RecommenderApp.kt
@@ -14,9 +14,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -75,6 +77,16 @@ fun ListScreen(
     Scaffold(
         topBar = {
             RecipeAppBar()
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { Unit }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "Refresh Recommendations"
+                )
+            }
         }
     ) {
         innerPadding ->
